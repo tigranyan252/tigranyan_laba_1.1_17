0fcb1f7eba71cb56ab207d3fef89a1cb7f5c00dd
su <su@DESKTOP-93TC6F5>
2021-10-20 12:42:34 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Лаб_работа_1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            try
+
+            {
+                double x, b, a, T, y;
+                Console.WriteLine("введите x:");
+                x = double.Parse(Console.ReadLine());
+                Console.WriteLine("введите b:");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("введите a:");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("введите y:");
+                y = double.Parse(Console.ReadLine());
+                T = Math.Sqrt(x + a - b) + Math.Log(y) / Math.Atan(b + a);
+                Console.WriteLine(string.Format("T = {0:F2}", T));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("введите корректное значение");
+            }
+
+        }
+
+    }
+
+}
+
+
